5f8fe9f3fb6f3efc20e8064936e3b5332c68b646
Paulo Ferreira <paulo.jesus@aluno.faculdadeimpacta.com.br>
2019-11-26 00:21:32 -0300

    Criando RequestParameters

---
+++ App/App/Common/Network/RequestParameters.swift
@@ -0,0 +1,17 @@
+//
+//  RequestParameters.swift
+//  App
+//
+//  Created by Paulo Ferreira de Jesus - PFR on 24/11/19.
+//  Copyright © 2019 Paulo Ferreira. All rights reserved.
+//
+
+import Foundation
+
+struct RequestParameters {
+    enum Key: String {
+        case apiKey = "api_key"
+        case language = "language"
+        case page = "page"
+    }
+}
