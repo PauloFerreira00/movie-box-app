dd88620ff78b29c68a6c4a731b1373f9588b21f2
Paulo Ferreira <paulo.jesus@aluno.faculdadeimpacta.com.br>
2019-11-26 11:38:34 -0300

    Atualizando DetailViewModel

--- App/App/Features/PopularMovies/View/Detail/View Model/DetailViewModel.swift
+++ App/App/Features/PopularMovies/View/Detail/View Model/DetailViewModel.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 Paulo Ferreira. All rights reserved.
 //
 
-
 enum DetailState {
     case loading
     case empty
@@ -17,10 +16,70 @@ enum DetailState {
 protocol DetailViewModelContract {
     func fetch(completion: @escaping (Result<Void, Error>) -> Void)
     var stateChange: (() -> Void)? { get set }
-    var currentState: PopularMoviesState { get }
-    var popularMovies: [Movie] { get }
+    var currentState: DetailState { get }
+    var detail: MovieDetail? { get }
 }
 
-protocol DetailViewModel: DetailViewModelContract {
-    
+class DetailViewModel: DetailViewModelContract {
+
+    var stateChange: (() -> Void)?
+    var currentState: DetailState = .loading {
+        didSet {
+            stateChange?()
+        }
+    }
+
+    let movie: Movie
+    var credits: MovieCredits?
+    var detail: MovieDetail?
+    let service: MovieService
+
+    init(movie: Movie, service: MovieService = MovieService.shared) {
+        self.movie = movie
+        self.service = service
+    }
+
+    func fetch(completion: @escaping (Result<Void, Error>) -> Void) {
+        currentState = .loading
+        service.fetchCredits(id: movie.id) { result in
+            switch result {
+            case .success(let credits):
+                self.currentState = .loaded
+                self.credits = credits
+                self.setupDetail(credits: credits)
+                return completion(.success(()))
+            case .failure(let error):
+                self.currentState = .error
+                return completion(.failure(error))
+            default: break
+            }
+        }
+    }
+
+    func setupDetail(credits: MovieCredits) {
+        let author = getAuthor()
+        let director = getDirector()
+        self.detail = MovieDetail(movie: self.movie, author: author, director: director)
+    }
+
+    func getAuthor() -> String {
+        var name: String = "Não informado"
+        credits?.crew.forEach { crew in
+            if crew.job == "Writer" {
+                name = crew.name
+            }
+        }
+        return name
+    }
+
+    func getDirector() -> String {
+        var name: String = "Não informado"
+        credits?.crew.forEach { crew in
+            if crew.job == "Director" {
+                name = crew.name
+            }
+        }
+        return name
+    }
+
 }
--- App/App/Features/PopularMovies/View/Detail/View/DetailViewController.swift
+++ App/App/Features/PopularMovies/View/Detail/View/DetailViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import NotificationBannerSwift
 
 class DetailViewController: UIViewController {
 
+    var viewModel: DetailViewModelContract?
+
+    static func instantiate(with viewModel: DetailViewModelContract) -> DetailViewController {
+        let viewController = DetailViewController(nib: R.nib.detailViewController)
+        viewController.viewModel = viewModel
+        return viewController
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetch()
+        print(viewModel?.detail)
+    }
 
+    func fetch() {
+        DispatchQueue.main.async { [weak self] in
+            self?.viewModel?.fetch(completion: { result in
+                switch result {
+                case .success:
+                    print()
+                case .failure(let error):
+                    let banner = FloatingNotificationBanner(title: "Error", subtitle: error.localizedDescription, style: .danger)
+                    banner.show(cornerRadius: 10)
+                }
+            })
+        }
     }
 
 }
